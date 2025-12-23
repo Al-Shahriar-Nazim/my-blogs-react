733f32d0b360c454367ceeaceab68421bef22e11
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2025-12-23 18:28:00 +0600

    added this project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="p-5">
+      {/* <h2 className="text-3xl">My Articales</h2> */}
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
